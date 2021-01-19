cdadb222c7b3b35f62c613005f8d4631aa82a588
LuisG8 <54091925+LuisG8@users.noreply.github.com>
2021-01-19 14:14:23 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
 public class Main{
-	public static void main(String args[0]){
+	public static void main(String args[]){
 		System.out.println("Hola brous.");
 		System.out.println("No se como entre aqui XD.");
+		System.out.println("Acaso neva en L.A? Acaso hay gabachos con placazo V.L? Nel, chale");
+	try{
+		File f = new File(args[0])
+		FileReader fr = new FileReader(f);
+		BufferedReader bf = new BufferedReader(fr);
+		Yylex lexer = new Yylex(br);
+		Parser parser = new Parser(lexer);
+		parser.init();
+		parser.programa();
+		if(parser.currentToken.equals(0)){
+			System.out.println("Cadena Aceptada");
+		}else{
+			System.out.println("Error sintactico");
+		}
+		br.close();
+
+	}catch(IOException e){
+		System.out.println("Error al abrir el archivo");
+	}
+
 		
   }
 }
