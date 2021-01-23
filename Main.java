6a33414cca7e69945dad96eb2d67a2fc98069dd6
lucalice <42328576+lucalice@users.noreply.github.com>
2021-01-22 21:31:12 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,32 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.File;
+import java.lang.*;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main{
-	public static void main(String args[]){
-		System.out.println("Hola brous.");
-		System.out.println("No se como entre aqui XD.");
-		System.out.println("Acaso neva en L.A? Acaso hay gabachos con placazo V.L? Nel, chale");
+	public static void main(String args[]) throws IOException{
+
 	try{
-		File f = new File(args[0])
+		File f = new File(args[0]);
 		FileReader fr = new FileReader(f);
 		BufferedReader bf = new BufferedReader(fr);
-		Yylex lexer = new Yylex(br);
+		Yylex lexer = new Yylex(bf);
 		Parser parser = new Parser(lexer);
 		parser.init();
 		parser.programa();
-		if(parser.currentToken.equals(0)){
+		if(parser.currentToken.clase == 0){
 			System.out.println("Cadena Aceptada");
 		}else{
 			System.out.println("Error sintactico");
 		}
-		br.close();
+		bf.close();
 
 	}catch(IOException e){
 		System.out.println("Error al abrir el archivo");
-	}
-
-		
+	}	
   }
 }
