a8910ae171637b8f7e3a0684ce3596ceb0555f9f
lucalice <42328576+lucalice@users.noreply.github.com>
2021-02-09 00:55:27 -0600

    Add files via upload

---
+++ TablaTipos.java
@@ -0,0 +1,27 @@
+public class TablaTipos{
+
+    int id;
+    String tipo;
+    int tamaño;
+    int numElementos;
+    int tipoBase;
+
+    public TablaTipos(){
+        
+    }
+    public TablaTipos(int id,String tipo, int tamaño, int numElementos,int tipoBase){
+        this.id = id;
+        this.tipo = tipo;
+        this.tamaño = tamaño;
+        this.numElementos = numElementos;
+        this.tipoBase = tipoBase;
+    }
+
+    /*
+    TT
+    id   nombre    tamaño ele tipoBase
+    0     "int"      4     0    ---
+    1    "array"     16    4     0
+
+    */
+}
