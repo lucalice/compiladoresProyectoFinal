30c8e66062791458479f222a624228aea232ef2c
lucalice <42328576+lucalice@users.noreply.github.com>
2021-02-10 01:10:29 -0600

    Update TablaSimbolos.java

--- TablaSimbolos.java
+++ TablaSimbolos.java
@@ -10,4 +10,13 @@ public class TablaSimbolos{
     public TablaSimbolos(){
 
     }
+
+    public TablaSimbolos(int posicion, String id, int tipo, int direccion, String var, int args){
+        this.posicion = posicion;
+        this.id = id;
+        this.tipo = tipo;
+        this.direccion = direccion;
+        this.var = var;
+        this.args = args;
+    }
 }
