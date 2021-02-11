714f0cb0000479a6167e899299fe29002e25f0ce
lucalice <42328576+lucalice@users.noreply.github.com>
2021-02-11 11:24:50 -0600

    Update Token.java

--- Token.java
+++ Token.java
@@ -2,20 +2,23 @@ public class Token{
     int clase;
     String valor;
     int tipo;
+    int line;
     
     public Token(){
         
     }
     
-    public Token(int clase, String valor){
+    public Token(int clase, String valor,int line){
       this.clase = clase;
       this.valor = valor;
+      this.line = line;
     }
 
-    public Token(int clase, String valor, int tipo){
+    public Token(int clase, String valor, int tipo,int line){
       this.clase = clase;
       this.valor = valor;
       this.tipo = tipo;
+      this.line = line;
     }
     
 }
